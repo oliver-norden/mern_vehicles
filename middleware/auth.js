f73e91bdcf5032ac3cbd87f4cddd36991994c7fe
Oliver Nordén <oliver@tapirens.se>
2019-09-08 13:01:49 +0200

    Created auth middleware to extract user from token

---
+++ middleware/auth.js
@@ -0,0 +1,25 @@
+const config = require('config');
+const jwt = require('jsonwebtoken');
+
+function auth(req, res, next) {
+    // Attempt to get token from request header
+    const token = req.header('x-auth-token');
+
+    // Check for token
+    if (!token) return res.status(401).json({ msg: 'No token' });
+
+    try {
+        // Verify token
+        const decodedToken = jwt.verify(token, config.get('jwtSecret'));
+
+        // Add user from token payload
+        req.user = decodedToken;
+
+        next();
+    }
+    catch (e) {
+        res.status(400).json({ msg: 'Invalid token' });
+    }
+}
+
+module.exports = auth;

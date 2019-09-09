oliver-norden/mern_vehicles

commit f71c83f5f0979c06131f4c8cd697dfc462fd3528

Sign out when token has expired (and action has been attempted)

client/src/actions/errorActions.js
@@ -1,6 +1,7 @@
-import { CREATE_ERROR, CLEAR_ERRORS } from "./types";
+import { CREATE_ERROR, CLEAR_ERRORS, USER_FAIL } from "./types";
 
-export const createError = (status, msg, id = null) => {
+export const createError = (status, msg, id = null) => dispatch => {
+    if (status === 401 ) dispatch(USER_FAIL);
     return {
         type: CREATE_ERROR,
         payload: {

commit 03bd780e38b54a549ba4dd9f1b28e07e582e2805

Fixed issues caused by f71c83f (No error messages)

client/src/actions/errorActions.js
@@ -1,7 +1,8 @@
-import { CREATE_ERROR, CLEAR_ERRORS, USER_FAIL } from "./types";
+import { CREATE_ERROR, CLEAR_ERRORS } from "./types";
+import { loadUser } from './authActions';
 
-export const createError = (status, msg, id = null) => dispatch => {
-    if (status === 401 ) dispatch(USER_FAIL);
+export const createError = (status, msg, id = null) => {
+    loadUser();
     return {
         type: CREATE_ERROR,
         payload: {